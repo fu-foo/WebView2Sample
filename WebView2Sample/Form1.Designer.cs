0f5746cb4087a0d1d4cef0a94592a63d6cbed508
fu <kaz.fuka@gmail.com>
2022-10-16 20:35:13 +0900

    first commit

---
+++ WebView2Sample/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Fu.WebView2Sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView2 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView2
+            // 
+            this.webView2.CreationProperties = null;
+            this.webView2.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView2.Location = new System.Drawing.Point(0, 0);
+            this.webView2.Name = "webView2";
+            this.webView2.Size = new System.Drawing.Size(800, 450);
+            this.webView2.TabIndex = 0;
+            this.webView2.ZoomFactor = 1D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webView2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.webView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView2;
+    }
+}
+
---
+++ WebView2Sample/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+
+
+namespace Fu.WebView2Sample
+{
+    /// <summary>
+    /// 最低限のWebView2サンプル(yahoo.co.jpを開く→検索文字列をセット→検索）
+    /// </summary>
+    public partial class Form1 : Form
+    {
+
+        /// <summary>
+        /// フォーム初期化
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitializeAsync();
+        }
+
+        /// <summary>
+        /// WebView2初期化
+        /// </summary>
+        async void InitializeAsync()
+        {
+            // WebView2の初期処理確認
+            await this.webView2.EnsureCoreWebView2Async(null);
+
+            // Yahoo検索画面
+            this.webView2.CoreWebView2.Navigate("https://yahoo.co.jp");
+
+            // 遷移完了のイベント追加
+            this.webView2.CoreWebView2.NavigationCompleted += this.webView2_NavigationCompleted;
+        }
+
+        /// <summary>
+        /// 遷移完了後の処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void webView2_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            if (e.IsSuccess)
+            {
+                // 検索文字列に"C#"をセット
+                this.webView2.ExecuteScriptAsync("document.getElementsByName('p').item(0).value = 'C#';");
+                // submit
+                this.webView2.ExecuteScriptAsync("document.getElementsByName('sf1').item(0).submit();");
+            }
+            else
+            {
+                // エラー処理
+            }
+        }
+    }
+}
